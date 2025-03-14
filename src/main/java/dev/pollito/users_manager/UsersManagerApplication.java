f2dc475ce19d57a8d0c181ce87e8d3976e922f94
Franco Becvort <franbecvort@gmail.com>
2025-03-14 10:14:47 +0000

    Initial commit

---
+++ src/main/java/dev/pollito/users_manager/UsersManagerApplication.java
@@ -0,0 +1,13 @@
+package dev.pollito.users_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersManagerApplication.class, args);
+	}
+
+}
