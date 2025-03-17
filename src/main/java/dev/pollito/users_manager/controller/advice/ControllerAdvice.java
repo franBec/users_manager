b2204e8244c07df5b208214b0c0413e816386aa5
Franco Becvort <franbecvort@gmail.com>
2025-03-17 13:18:42 +0000

    error handling

---
+++ src/main/java/dev/pollito/users_manager/controller/advice/ControllerAdvice.java
@@ -0,0 +1,49 @@
+package dev.pollito.users_manager.controller.advice;
+
+import io.opentelemetry.api.trace.Span;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.util.NoSuchElementException;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+@RestControllerAdvice
+@Slf4j
+public class ControllerAdvice {
+
+  @NotNull private static ProblemDetail problemDetail(@NotNull Exception e, HttpStatus status) {
+    String exceptionSimpleName = e.getClass().getSimpleName();
+    log.error("{} being handled", exceptionSimpleName, e);
+    ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, e.getLocalizedMessage());
+    problemDetail.setTitle(exceptionSimpleName);
+    problemDetail.setProperty("timestamp", DateTimeFormatter.ISO_INSTANT.format(Instant.now()));
+    problemDetail.setProperty("trace", Span.current().getSpanContext().getTraceId());
+    return problemDetail;
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ProblemDetail handle(@NotNull Exception e) {
+    return problemDetail(e, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler(NoResourceFoundException.class)
+  public ProblemDetail handle(@NotNull NoResourceFoundException e) {
+    return problemDetail(e, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+  public ProblemDetail handle(@NotNull MethodArgumentTypeMismatchException e) {
+    return problemDetail(e, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(NoSuchElementException.class)
+  public ProblemDetail handle(@NotNull NoSuchElementException e) {
+    return problemDetail(e, HttpStatus.NOT_FOUND);
+  }
+}
